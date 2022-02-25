22c063845dad897f6ce44ddd6cbe3da7151e3b69
Matt Payne <Payne@MattPayne.org>
2022-02-25 08:34:25 -0600

    wip compute total spent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from replit import web
+from collections import Counter
 
 app = Flask(__name__)
 
@@ -13,18 +15,49 @@ def index():
 def hello(name=None):
   return render_template("hello.html", name=name)
   
-@app.route("/home/")
-def home():
-  prices = {
-      'Rice Krispy Treats': 0.21,
-      'candy': 0.50,
-      'Propel': 0.45,
-      'coke': 0.40,
-      'celsius': 1.12
-  }
-  return render_template("home.html", prices = prices)
-  
+prices = {
+    'Rice Krispy Treats': 0.21,
+    'candy': 0.50,
+    'Propel': 0.45,
+    'coke': 0.40,
+    'celsius': 1.12
+}
 
+@app.route("/home/")
+@web.authenticated
+def home():
+  users_database = web.UserStore().current
+  purchases = users_database.get('purchases', [])
+  total_spent, itemization = compute_total_spent(purchases)
+  return render_template("home.html", prices = prices, total_spent = total_spent, itemization = itemization)
+
+def compute_total_spent(purchases):
+  items = [item for item in purchases]
+  c = Counter(items)
+  items_to_quantity_bought = { item: count for item, count in c.most_common() }
+  ts = 0
+  for item, count in items_to_quantity_bought.items():
+    ts -= float(prices[item]) * count
+  return ts, items_to_quantity_bought
+
+@app.route("/buy")
+@web.authenticated
+def buy():
+  item = request.args.get('item')
+  users_database = web.UserStore().current
+  purchases = users_database.get('purchases', [])
+  purchases.append(item)
+  users_database['purchases'] = purchases
+  return redirect(url_for("home"))
+
+  
+@app.route("/deposit/")
+def deposit():
+  return render_template("deposit.html")
+
+@app.route("/history/")
+def history():
+  return render_template("history.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
